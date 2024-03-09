4b1bb206a2ad9be680aa17a100241163cf96c92f
Nix3732 <eaz055@mail.ru>
2024-03-09 15:29:22 +0200

    1

---
+++ Ellips.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <iostream>
+
+class Ellipse
+{
+    int a;
+    int b;
+
+public:
+    Ellipse() = default;
+    Ellipse(int l, int s);
+
+    void setALL(int l, int s)
+    {
+        if (l < 0 || s < 0)
+        {
+            std::cout << "Negative semiaxis" << std::endl;
+        }
+
+        a = l;
+        b = s;
+    }
+
+    float Perimeter()
+    {
+        return 2 * acos(-1) * sqrt((pow(a, 2) + pow(b, 2)) / 2);
+    }
+
+    float Square()
+    {
+        return acos(-1) * a * b;
+    }
+};
--- L2.cpp
+++ L2.cpp
@@ -1,40 +1,6 @@
 ﻿#include <iostream>
 #include <cmath>
-
-class Ellipse
-{
-    int a;
-    int b;
-
-public:
-    Ellipse() = default;
-    Ellipse(int l, int s)
-    {    
-        setL(l, s);
-    }
-
-    void setALL(int l, int s)
-    {
-        if (l < 0 or s < 0)
-        {
-            std::cout << "Negative semiaxis" << std::endl;
-        }
-
-        a = l;
-        b = s;
-    }
-
-    float Perimeter()
-    {   
-        return 2 * acos(-1) * sqrt((pow(a, 2) + pow(b,2))/2);
-    }
-
-    float Square()
-    {
-        return acos(-1) * a * b;
-    }
-};
-
+#include "Ellips.hpp"
 
 int main()
 {   
