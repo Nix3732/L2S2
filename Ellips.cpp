da38aa9ff8c0a8f21e8235f28151ed026fc1de49
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-03-11 13:51:25 +0200

    Update Ellips.cpp

--- Ellips.cpp
+++ Ellips.cpp
@@ -5,22 +5,22 @@ namespace ez
 {
 	void Ellipse::setALL(int l, int s)
 	{
-        if (l < 0 || s < 0)
-        {
-            std::cout << "Negative semiaxis" << std::endl;
-        }
-
-        a = l;
-        b = s;
+	if (l < 0 || s < 0)
+	{
+	    std::cout << "Negative semiaxis" << std::endl;
+	}
+	
+	a = l;
+	b = s;
 	}
 
-    float Ellipse::Perimeter()
-    {
-        return 2 * acos(-1) * sqrt((pow(a, 2) + pow(b, 2)) / 2);
-    }
+    	float Ellipse::Perimeter()
+    	{
+        	return 2 * acos(-1) * sqrt((pow(a, 2) + pow(b, 2)) / 2);
+    	}
 
-    float Ellipse::Square()
-    {
-        return acos(-1) * a * b;
-    }
+    	float Ellipse::Square()
+    	{
+        	return acos(-1) * a * b;
+    	}
 }
